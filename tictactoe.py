7f64f089da76e2114d9212cdaa20f6a2189e1010
amberbrown4 <zasalarian2000@gmail.com>
2020-04-13 17:52:37 +0430

    Implemented utility function

--- tictactoe.py
+++ tictactoe.py
@@ -92,6 +92,12 @@ def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
+    if winner(board) == X:
+        return 1
+    elif winner(board) == O:
+        return -1
+    else:
+        return 0
     # raise NotImplementedError
 
 
